1e1920fa8dce8f6b9f18ac9b0f185f06171013ad
Sergey Kulchytski <serg@ib.by>
2021-05-01 10:07:06 +0300

    implement base table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,17 +4,41 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners);
     this.name = options.name || '';
+    this.emmiter = options.emmiter;
+    this.unsubscribers = [];
+
+    this.prepare();
   }
+
+  // настраиваем компонент до init
+  prepare() {}
+
   // return a template of a component
   toHTML() {
     return '';
   }
 
+  // уведомляем слушателей про событие event
+  $emit(event, ...args) {
+    this.emmiter.emit(event, ...args);
+  }
+
+  // Подписываемся на событие event
+  $on(event, fn) {
+    const unsub = this.emmiter.subscribe(event, fn);
+    this.unsubscribers.push(unsub);
+  }
+
+  // инициализуруем компонент
+  // добавляем dom слушателей
   init() {
     this.initDomListeners();
   }
 
+  // Удаляем компонент
+  // чистим слушателей
   destroy() {
     this.removeDomListeners();
+    this.unsubscribers.forEach(unsub => unsub());
   }
 }
